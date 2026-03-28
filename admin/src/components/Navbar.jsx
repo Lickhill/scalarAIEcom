caf8ee04419e89806e2aa35fd26b3a80180ba0c2
Likhil N Maiya <134200383+Lickhill@users.noreply.github.com>
2026-03-28 22:49:49 +0530

    logo in admin page

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import {assets} from '../assets/assets'
+import React from "react";
 
-const Navbar = ({setToken}) => {
-  return (
-    <div className='flex items-center py-2 px-[4%] justify-between'>
-        <img className='w-[max(15%,80px)]' src={assets.brand_logo} alt="" />
-        <button onClick={()=> setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
-    </div>
-  )
-}
+const Navbar = ({ setToken }) => {
+	return (
+		<div className="flex items-center py-2 px-[4%] justify-between">
+			<div className="bg-yellow-400 px-5 py-2.5 rounded-lg font-bold text-lg">
+				<span className="text-blue-600">L</span>
+				<span className="text-black">ICKHILL</span>
+			</div>
+			<button
+				onClick={() => setToken("")}
+				className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+			>
+				Logout
+			</button>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
